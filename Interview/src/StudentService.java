e5c3c75626838fe7096e7c716fe900ed14b8874d
christy2876 <christyl.2876@gmail.com>
2023-03-21 11:52:33 -0700

    Revert "2023"
    This reverts commit 27f9f4b61b6fea06b2a3e333fedad6fcbe787a14.

--- Interview/src/StudentService.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface StudentService {
-
-    Student getById(String input);
-    void createStudent(String input);
-    List<Student> getAll();
-    List<Student> getAllSorted();
-}
